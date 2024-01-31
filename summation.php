9c7a864d5ec4c088e3aba00b401ffae89262a13a
djchidi4 <158151469+djchidi4@users.noreply.github.com>
2024-01-31 15:10:31 +0100

    Add files via upload

---
+++ summation.php
@@ -0,0 +1,39 @@
+<?php
+function categorizeAndSummarizeCSV($csvFilePath) {
+    $categories = [];
+
+    if (($handle = fopen($csvFilePath, "r")) !== FALSE) {
+        fgetcsv($handle, 1000, ",");
+
+        while (($data = fgetcsv($handle, 1000, ",")) !== FALSE) {
+            $industry = $data[3];
+            $financialValue = floatval($data[8]);
+
+            if (!isset($categories[$industry])) {
+                $categories[$industry] = $financialValue;
+            } else {
+                $categories[$industry] += $financialValue;
+            }
+        }
+        fclose($handle);
+    }
+
+    echo "<table border='1'>
+            <tr>
+                <th>Industry</th>
+                <th>Financial Value Summation</th>
+            </tr>";
+
+    foreach ($categories as $industry => $value) {
+        echo "<tr>
+                <td>{$industry}</td>
+                <td>{$value}</td>
+            </tr>";
+    }
+
+    echo "</table>";
+}
+
+categorizeAndSummarizeCSV('test.csv');
+
+?>
